5751b2199ea821083de4192103cc738eaf4da29f
Andrew Kraut <akraut@gmail.com>
2009-01-05 17:56:33 -0600

    skeleton of expand

--- bitly.py
+++ bitly.py
@@ -15,9 +15,15 @@ def shorten(longUrl, **kwargs):
 	
 	Parameters:
 		longUrl (required): URL to shorten
+<<<<<<< HEAD:bitly.py
 		                    ie: http://cnn.com
 		keyword (optional): preferred keyword
 		                    ie: cnn
+=======
+		                    ie: shorten('http://cnn.com')
+		keyword (optional): preferred keyword
+		                    ie: shorten('http://cnn.com', keyword='cnn')
+>>>>>>> 94612f5... skeleton of expand:bitly.py
 	"""
   kwargs.update({
     'version': API_VERSION,
@@ -29,8 +35,25 @@ def shorten(longUrl, **kwargs):
   url = API_BASE + '/shorten?' + urllib.urlencode(kwargs)
   result = simplejson.load(urllib.urlopen(url))
   if 'ERROR' in result:
-    raise BitlyAPIError, result['Error']
+    raise BitlyAPIError, result['errorMessage']
   if result['results'][longUrl]['shortKeywordUrl'] == "":
     return result['results'][longUrl]['shortUrl']
   else:
     return result['results'][longUrl]['shortKeywordUrl']
+
+def expand(**kwargs):
+	"""
+	Given a bit.ly url or hash(es), return the long source url(s).
+	
+	Parameters:
+	  shortUrl: a single URL to expand
+	            ie: expand('http://bit.ly/QJhM')
+	  hash: one or more URL hashes to expand
+	            ie: expand('QJhM,3el7')
+	"""
+	kwargs.update({
+		'version': API_VERSION,
+		'format': 'json',
+		'login': API_LOGIN,
+		'apiKey':API_KEY,
+	})
